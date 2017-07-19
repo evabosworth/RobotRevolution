6062c2d23a0b112e38be78c807a7ef9a21ba2ca9
Evelyn Bosworth <evabosworth@gmail.com>
2017-07-19 16:39:17 -0700

    updated required status enum

--- RobotRevolution/Assets/Scripts/ScriptBuilding/ScriptElement.cs
+++ RobotRevolution/Assets/Scripts/ScriptBuilding/ScriptElement.cs
@@ -1,11 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AssemblyCSharp;
 
 public abstract class ScriptElement {
 	private List<ScriptElement> beforeElements = new List<ScriptElement>();
 	private List<ScriptElement> afterElements = new List<ScriptElement>();
+	private ScriptElementRequiredStatus elementRequiredStatus = ScriptElementRequiredStatus.START;
 
+	/**
+	 * getScriptRequiredStatus 
+	 * return ScriptElementRequiredStatus, denotes if an element is required
+	 */
+	public ScriptElementRequiredStatus getScriptRequiredStatus(){
+		return elementRequiredStatus;
+	}
 
+	public List<ScriptElement> getBeforeElements(){
+		return beforeElements;
+	}
 
+	public List<ScriptElement> getAfterElements(){
+		return afterElements;
+	}
+
+	//Needs to not be object, second one will be params, such as select allies and such
+	public abstract List<Object> getAllElementOptions(List<Object> parameters);
 }
---
+++ RobotRevolution/Assets/Scripts/ScriptBuilding/ScriptElementRequiredStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public enum ScriptElementRequiredStatus
+	{
+		REQUIRED,
+		OPTIONAL,
+		START
+	}
+}
+
