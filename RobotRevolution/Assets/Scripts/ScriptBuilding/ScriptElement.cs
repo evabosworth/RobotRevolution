7b69eada85ffbe55c38dd565fe58cf78a7e4475e
Evelyn Bosworth <evabosworth@gmail.com>
2017-07-18 17:40:23 -0700

    Updated to newest unity, added starting scriptElement Super class

---
+++ RobotRevolution/Assets/Scripts/ScriptBuilding/ScriptElement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ScriptElement {
+	private List<ScriptElement> beforeElements = new List<ScriptElement>();
+	private List<ScriptElement> afterElements = new List<ScriptElement>();
+
+
+
+}
